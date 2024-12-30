2b63a78afa5fc9e896651adf2a95e021f26d2494
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-12-30 19:14:26 +0100

    setup colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-purple": "var(--main-purple)",
+        "main-purple-hover": "var(--main-purple-hover)",
+        "light-grey": "var(--light-grey)",
+        "medium-grey": "var(--medium-grey)",
+        "dark-grey": "var(--dark-grey)",
+        "very-dark-grey": "var(--very-dark-grey)",
+        "light-lines": "var(--light-lines)",
+        "dark-lines": "var(--dark-lines)",
+        "red": "var(--red)",
+        "red-hover": "var(--red-hover)",
+      }
+    },
   },
   plugins: [],
 }
