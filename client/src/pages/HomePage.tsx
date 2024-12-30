dacfd618e75e9b7332a74ae0cfc88dbb18621715
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-12-30 20:41:10 +0100

    home page layout

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,6 +1,25 @@
 function HomePage() {
   return (
-    <h1>Home Page</h1>
+    <div>
+        <div className="flex">
+            <aside className="hidden md:block">
+                <div className="sticky top-0 bg-white h-20">
+                    <p>logo</p>
+                </div>
+                <div className="sticky top-20 w-[260px] h-[calc(100vh-80px)] bg-white">
+                    <p>Sidebar</p>
+                </div>
+            </aside>
+            <main className="grow">
+                <div className="sticky top-0 bg-white">
+                    <p>topbar</p>
+                </div>
+                <div>
+                    main content
+                </div>
+            </main>
+        </div>
+    </div>
   )
 }
 
