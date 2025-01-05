319135558bc045ff84a48e5ea4e09b2204fcb3ae
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-01-05 16:07:27 +0100

    added input ui

---
+++ client/src/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded border border-medium-grey bg-white text-black px-4 py-2 text-2xs placeholder:text-medium-grey focus-visible:outline-none focus-visible:border-main-purple disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
