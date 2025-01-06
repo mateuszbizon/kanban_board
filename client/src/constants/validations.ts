be33e4267738fa0925c0d4c9445cc97eeccddeb1
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-01-06 17:27:28 +0100

    board validation

---
+++ client/src/constants/validations.ts
@@ -0,0 +1,3 @@
+export const BOARD_MAX_LENGTH = "Board max length 20"
+export const COLUMN_MAX_LENGTH = "Column max length 20"
+export const MINIMUM_LENGTH = "Can't be empty"
---
+++ client/src/validations/boardSchema.ts
@@ -0,0 +1,9 @@
+import { BOARD_MAX_LENGTH, COLUMN_MAX_LENGTH, MINIMUM_LENGTH } from "@/constants/validations"
+import { z } from "zod"
+
+export const boardSchema = z.object({
+    name: z.string().min(1, MINIMUM_LENGTH).max(20, BOARD_MAX_LENGTH),
+    columns: z.array(z.object({
+        name: z.string().min(1, MINIMUM_LENGTH).max(20, COLUMN_MAX_LENGTH)
+    }))
+})
