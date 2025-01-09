a93f482ed4c7bfb7d5570672cfa748c6b412a9f1
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-01-09 19:00:44 +0100

    create and edit board with board messages

--- client/src/components/board/BoardEmpty.tsx
+++ client/src/components/board/BoardEmpty.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import { Button } from '../ui/button'
+import EditBoard from './EditBoard'
 
 function BoardEmpty() {
+    const [isEditBoardOpen, setIsEditBoardOpen] = useState(false)
+
   return (
     <div className="flex flex-col gap-5">
         <p className="text-lg text-medium-grey text-center">This board is empty. Create a new column to get started.</p>
         <div className="flex justify-center">
-            <Button>+ Add New Column</Button>
+            <Button onClick={() => setIsEditBoardOpen(true)}>+ Add New Column</Button>
         </div>
+        <EditBoard isEditBoardOpen={isEditBoardOpen} setIsEditBoardOpen={setIsEditBoardOpen} />
     </div>
   )
 }
--- client/src/components/board/NoBoards.tsx
+++ client/src/components/board/NoBoards.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
 import { Button } from "../ui/button"
+import Modal from "../common/Modal"
+import BoardForm from "../forms/BoardForm"
 
 function NoBoards() {
+    const [isCreateBoardOpen, setIsCreateBoardOpen] = useState(false)
+
   return (
     <div className="flex flex-col gap-5">
         <p className="text-lg text-medium-grey text-center">There are no boards yet. Create a new board to get started.</p>
         <div className="flex justify-center">
-            <Button>+ Add New Board</Button>
+            <Button onClick={() => setIsCreateBoardOpen(true)}>+ Add New Board</Button>
         </div>
+        <Modal isOpen={isCreateBoardOpen} onClose={() => setIsCreateBoardOpen(false)} isModalDelete={false}>
+            <BoardForm />
+        </Modal>
     </div>
   )
 }
