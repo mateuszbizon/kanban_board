07fc0d9cfae17749c861dd984833f4ca8c813c1b
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-01-10 12:29:41 +0100

    added textarea ui

---
+++ client/src/components/ui/textarea.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.ComponentProps<"textarea">
+>(({ className, ...props }, ref) => {
+  return (
+    <textarea
+      className={cn(
+        "flex min-h-[80px] w-full rounded border border-medium-grey bg-white text-black px-4 py-2 text-2xs placeholder:text-medium-grey focus-visible:outline-none focus-visible:border-main-purple disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  )
+})
+Textarea.displayName = "Textarea"
+
+export { Textarea }
