a1f7002280b6808b43e830577ecf0e9ae4fe98bc
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-01-24 16:36:47 +0100

    added drag drop icon

--- client/src/components/cards/TaskCard.tsx
+++ client/src/components/cards/TaskCard.tsx
@@ -4,6 +4,8 @@ import Modal from '../common/Modal';
 import CurrentTask from '../task/CurrentTask';
 import { useDraggable } from '@dnd-kit/core';
 import { useState } from 'react';
+import { Button } from '../ui/button';
+import DragDropIcon from '../icons/DragDropIcon';
 
 type TaskCardProps = {
     task: Task;
@@ -19,12 +21,13 @@ function TaskCard({ task }: TaskCardProps) {
   return (
     <div
         ref={setNodeRef}
-        {...listeners}
-        {...attributes}
         style={{ transform: transform ? `translate(${transform.x}px, ${transform.y}px)` : undefined }}
     >
         <div className='group bg-white dark:bg-dark-grey p-main rounded-lg space-y-3 shadow-main cursor-pointer' onClick={() => setIsCurrentTaskOpen(true)}>
-            <p className='text-2sm text-black dark:text-white group-hover:text-main-purple transition-colors'>{task.title}</p>
+            <div className='flex justify-between items-center gap-5'>
+                <p className='text-2sm text-black dark:text-white group-hover:text-main-purple transition-colors'>{task.title}</p>
+                <Button {...listeners} {...attributes} variant={"transparent"} size={"icon"} className='shrink-0 p-0'><DragDropIcon /></Button>
+            </div>
             <p className='text-xs text-medium-grey'>{getSubtasksCompletedLength(task.subtasks)} of {task.subtasks.length} subtasks</p>
         </div>
         <Modal isOpen={isCurrentTaskOpen} onClose={() => setIsCurrentTaskOpen(false)} isModalDelete={false}>
